024701bb2622c8df547c54f3d1d5f2d3789dcb2c
Linda <lsanc0043@gmail.com>
2022-08-04 14:04:07 -0700

    Add DOM manipulation

---
+++ recipe/script.js
@@ -0,0 +1,55 @@
+const ingredientList = [
+    "1 lb of ground meat", "choice of baby spinach of napa cabbage",
+"wonton/dumpling wrappers", "3 spoons of soy sauce", 
+"1 spoon of dark soy sauce", "2 spoons of corn starch", 
+"1 teaspoon of sugar", "a pinch of salt", "a pinch of white pepper", 
+"a cup of water", "optional: mushrooms or green onions for fragrance"];
+
+const navBarList = [
+    "Ingredients", "Steps to Make", "Links to Other Wonton Soup Recipes", "About Me"
+]
+
+function check(target, phrase) { // adds checkboxes
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.setAttribute('type', "checkbox")
+    label.innerHTML = phrase;
+    // label.style.textDecoration = "none";
+    
+    input.addEventListener('change', function () {
+        if (this.checked) {
+            label.style.textDecoration = "line-through";
+        } 
+        else {
+            label.style.textDecoration = "none";
+        }
+    })
+
+    target.appendChild(input);
+    target.appendChild(label);
+}
+
+for (let i = 0; i < ingredientList.length; i++) {
+    const li = document.createElement('li');
+    check(li, ingredientList[i]);
+    document.getElementById("Ingredients").appendChild(li);
+}
+
+for (let i = 0; i < navBarList.length; i++) {
+    const label = document.createElement('a');
+    label.innerText = navBarList[i];
+    label.style.margin = "0px 70px 0px";
+    label.addEventListener('mouseover', function() {
+        label.style.color = "white";
+    })
+    label.addEventListener('mouseout', function() {
+        label.style.color = "black";
+    })
+    label.addEventListener('click', function() {
+        label.setAttribute('href', `#${navBarList[i]}`);
+        label.style.textDecoration = "none";
+    })
+
+    document.getElementById("nav").appendChild(label);
+}
+
