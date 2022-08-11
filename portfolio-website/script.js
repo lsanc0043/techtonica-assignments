ac38908b4b43abf6490effb0a00af525cbd20c41
lsanc0043 <lsanc0043@gmail.com>
2022-08-11 13:55:27 -0700

    Format home page, add form

--- portfolio-website/script.js
+++ portfolio-website/script.js
@@ -1,21 +1,18 @@
 const navBarList = [
-    "Ingredients", "Steps to Make", "Links to Other Wonton Soup Recipes", "About Me"
+    "About Me", "Projects", "Contact"
 ]
 
 for (let i = 0; i < navBarList.length; i++) {
     const label = document.createElement('a');
     label.innerText = navBarList[i];
     label.addEventListener('mouseover', function() {
-        label.style.color = "white";
+        label.style.color = "black";
     })
     label.addEventListener('mouseout', function() {
-        label.style.color = "black";
+        label.style.color = "white";
     })
     label.addEventListener('click', function() {
         label.setAttribute('href', `#${navBarList[i].split(" ").join("")}`);
-        if (i === 3) {
-            label.setAttribute('href', "./aboutme.html");
-        }
         label.style.textDecoration = "none";
     })
 
