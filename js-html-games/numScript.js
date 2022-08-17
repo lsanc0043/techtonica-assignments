a246a19886a7735c27d3decb50b94569089d2363
lsanc0043 <lsanc0043@gmail.com>
2022-08-16 18:15:59 -0700

    Finish guessnum

--- js-html-games/numScript.js
+++ js-html-games/numScript.js
@@ -5,53 +5,129 @@ const range = document.getElementById("range");
 const statement = document.getElementById("statement");
 const bttn = document.getElementById("set");
 const hint = document.getElementById("hint");
-const bounds = [0, 0];
+let bounds = [];
 let correctNum = 0;
 
-custom.addEventListener('click', function() {
-    range.style.display = "block";
-})
+function clear() {
+    bgColor("white");
+    setText("");
+}
 
-random.addEventListener('click', function() {
+function bgColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function setText(text) {
+    hint.innerText = text;
+}
+
+function addFormSubmit(corr) {
+    numForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const guess = document.getElementById("guess").value;
+        checkNum(guess, corr);
+    })
+}
+
+function hotOrCold(num) {
+    if (num === 0) {
+        bgColor("#8dcc9e");
+        setText("CORRECT! GOOD JOB!");
+    }
+    else if (num > 0 && num <= 10) {
+        bgColor("#e8f781");
+        setText("Close! Keep going! (within 10)");
+    }
+    else if (num > 10 && num <= 100) {
+        bgColor("#f0ce7a");
+        setText("Not quite! (within 100)");
+    }
+    else if (num > 100 && num <= 1000) {
+        bgColor("#d4733b");
+        setText("Not quite! (within 1000)");
+    }
+    else {
+        bgColor("#d14134");
+        setText("Within range, but you're way off!");
+    }
+}
+
+function upDown(num) {
+    if (num === 0) {
+        bgColor("#8dcc9e");
+        setText("CORRECT! GOOD JOB!");
+    }
+    else if (num > 0) {
+        bgColor("#e8f781");
+        setText("Go down!");
+    }
+    else if (num < 0) {
+        bgColor("#f0ce7a");
+        setText("Go up!");
+    }
+}
+
+function checkNum(val, answer) {
+    if (!isNaN(val)) {
+        if (val >= (bounds[0]) && val <= (bounds[1])) { // within bounds
+            const diff = (val - answer);
+            upDown(diff);
+            // hotOrCold(Math.abs(diff));
+        }
+        else {
+            bgColor("red");
+            setText("Outside of Bounds! Try again.")
+        }
+    }
+    else {
+        bgColor("red");
+        setText("Not a number! Try again.");
+    }
+}
+
+function randomize() {
+    clear();
     const num1 = Math.floor(Math.random() * 10000);
     const num2 = Math.floor(Math.random() * 10000);
     bounds[0] = Math.min(num1, num2);
     bounds[1] = Math.max(num1, num2);
     statement.innerHTML = `The number is between <strong>${bounds[0]}</strong> and <strong>${bounds[1]}</strong>`;
     numForm.style.display = "block";
-    correctNum = Math.floor(Math.random()*bounds[1] + bounds[0]);
+    correctNum = Math.floor(Math.random()*(bounds[1]-bounds[0]) + bounds[0]);
+    console.log(correctNum);
+    
+    addFormSubmit(correctNum);
+}
+
+custom.addEventListener('click', function() {
+    range.style.display = "block";
 })
+
+random.addEventListener('click', function() {
+    randomize();
+});
 
 bttn.addEventListener('click', function() {
     const lower = document.getElementById("lower").value;
     const upper = document.getElementById("upper").value;
     statement.innerHTML = `The number is between <strong>${lower}</strong> and <strong>${upper}</strong>`;
-    numForm.style.display = "block";
-    bounds[0] = lower;
-    bounds[1] = upper;
-})
-
-console.log(correctNum);
-
-numForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const guess = document.getElementById("guess").value;
-    console.log(guess);
-    if (!isNaN(guess)) {
-        if (guess > Number(bounds[0]) && guess < Number(bounds[1])) {
-            const diffLower = guess - Number([bounds[0]]);
-            const diffUpper = Number(bounds[1]) - guess;
-            hint.style.color = "green";
-            hint.innerText = "Close!"
+    bounds[0] = Number(lower);
+    bounds[1] = Number(upper);
+    correctNum = Math.floor(Math.random()*(bounds[1]-bounds[0]) + bounds[0]);
+    console.log(correctNum);
+    if (!isNaN(bounds[0] && !isNaN(bounds[1]))) {
+        if (bounds[1] < bounds[0]) {
+            bgColor("red");
+            setText("Upper bound cannot be less than lower bound");
         }
         else {
-            hint.style.color = "red";
-            hint.innerText = "Outside of Bounds! Try again."
+            numForm.style.display = "block";
+            clear();
+            addFormSubmit(correctNum);
         }
     }
     else {
-        hint.style.color = "red";
-        hint.innerText = "Not a number! Try again."
+        bgColor("red");
+        setText("Not a number! Try again.");
     }
-
 })
