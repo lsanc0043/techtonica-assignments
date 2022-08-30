86e5aeec76588184ab27e634548b7c4c52c682e7
lsanc0043 <lsanc0043@gmail.com>
2022-08-30 15:17:21 -0700

    rest API practice

---
+++ myrestapi/books.js
@@ -0,0 +1,22 @@
+const BOOKS = [
+  {
+    isbn: "0553120441",
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    format: "paperback",
+  },
+  {
+    isbn: "0553120441",
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    format: "paperback",
+  },
+  {
+    isbn: "0553120441",
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    format: "paperback",
+  },
+];
+
+export default BOOKS;
