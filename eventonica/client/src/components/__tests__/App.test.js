4c7cfb699599b6e0a76e3e6a535f6d13b750b7cf
lsanc0043 <lsanc0043@gmail.com>
2022-09-23 10:23:00 -0700

    Remove duplicate folder

--- eventonica/client/src/components/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { screen, render } from "@testing-library/react";
-import App from "../../App";
-
-describe("App", () => {
-  test("should render App", () => {
-    render(<App />);
-    screen.debug();
-  });
-});
--- eventonica/client/src/components/__tests__/Events.test.js
+++
@@ -1,9 +0,0 @@
-import { screen, render } from "@testing-library/react";
-import EventManagement from "../Events";
-
-describe("Events", () => {
-  test("should render Events component", () => {
-    render(<EventManagement />);
-    screen.debug();
-  });
-});
